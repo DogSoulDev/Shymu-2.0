1b2ce6842bcc1e6934c18d0090c11ea6a2c97570
Javier Fernández <dogsouldev@gmail.com>
2022-10-22 17:50:39 +0200

    add Login functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { Login, Home } from "./components";
+import { app } from "./config/firebase.config";
 import "./App.css";
+import { getAuth } from "firebase/auth";
 
 function App() {
+	const firebaseAuth = getAuth(app);
+	const navigate = useNavigate();
+
+	const [auth, setAuth] = useState(
+		false || window.localStorage.getItem("auth") === "true",
+	);
+
+	useEffect(() => {
+		firebaseAuth.onAuthStateChanged((userCred) => {
+			if (userCred) {
+				userCred.getIdToken().then((token) => {
+					console.log(token);
+				});
+			} else {
+				setAuth(false);
+				window.localStorage.setItem("auth", "false");
+				navigate("/login");
+			}
+		});
+	}, []);
+
 	return (
 		<div className='w-screen h-screen bg-primary flex justify-center items-center'>
 			<Routes>
-				<Route path='/login' element={<Login />} />
+				<Route path='/login' element={<Login setAuth={setAuth} />} />
 				<Route path='/*' element={<Home />} />
 			</Routes>
 		</div>
--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { app } from "../../config/firebase.config";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { FcGoogle } from "react-icons/fc";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => {
+const Login = ({setAuth}) => {
+	const firebaseAuth = getAuth(app);
+	const provider = new GoogleAuthProvider();
+	const navigate = useNavigate();
+
+	const loginWithGoogle = async () => {
+		await signInWithPopup(firebaseAuth, provider).then((userCred)=>{
+			console.log(userCred);
+		});
+	};
 	return (
 		<div className='relative w-screen h-screen flex justify-center items-center'>
 			<div className='absolute insert-0 bg-darkOverlay flex items-center justify-center p-4'>
 				<div className='w-full md:w-375 p-4 bg-lightOverlay shadow-2xl rounded-md backdrop-blur-md flex flex-col items-center justify-center'>
-					<div className='flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-cardOverlay cursos-pointer hover:bg-card hover:shadow-md duration-100 ease-in-out transition-all'>
-						<FcGoogle className='text-xl'/>
+					<div
+						className='flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-cardOverlay cursos-pointer hover:bg-card hover:shadow-md duration-100 ease-in-out transition-all'
+						onClick={loginWithGoogle}
+					>
+						<FcGoogle className='text-xl' />
 						Sign in with Google
 					</div>
 				</div>
