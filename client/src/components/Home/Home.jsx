5169587e21b3707851e0e7f9c0d75a06670a5387
Javier Fernández <dogsouldev@gmail.com>
2022-10-23 12:13:56 +0200

    Create logo rute on Home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import Header from "../Header/Header";
+import * as React from "react";
+// import Header from "../Header/Header";
+import { NavLink } from "react-router-dom";
+//*Info del NavLink en https://reactrouter.com/en/main/components/nav-link
+import { Logo } from "../../assets/img";
 
 const Home = () => {
 	return (
 		<div className='w-full h-auto flex flex-col items-center justify-center bg-primary '>
-			<Header />
+			<header className='flex items-center w-full p-4 md:py-2 md:px-6'>
+				<NavLink to='/'>
+					<img src={Logo} alt='Logo' className='w-16' />
+				</NavLink>
+			</header>
 		</div>
 	);
 };
