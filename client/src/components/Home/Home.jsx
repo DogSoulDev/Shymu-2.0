19cb1fd46bf83d97b86bfd5d69f5ab36e89f6d08
Alejandro Gaerste <alejandrogaerste411@gmail.com>
2022-10-27 18:02:39 +0200

    Feature: Add component About us, premium and Music

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -6,7 +6,9 @@ import { SongCard } from "../Dashboard/DashboardSongs";
 import Filter from "../Filter/Filter";
 import Header from "../Header/Header";
 import SearchBar from "../SearchBar/SearchBar";
+import Footer from "../Footer/Footer";
 import { motion } from "framer-motion";
+import { ArrowTopRightOnSquareIcon } from '@heroicons/react/20/solid'
 
 const Home = () => {
  const [
@@ -105,123 +107,7 @@ const Home = () => {
      </div>
     </div>
    </div>
-   <div className='relative bg-white py-16'>
-    <div
-     className='absolute inset-x-0 top-0 hidden h-1/2 bg-gray-50 lg:block'
-     aria-hidden='true'
-    />
-    <div className='mx-auto max-w-7xl bg-indigo-600 lg:bg-transparent lg:px-8'>
-     <div className='lg:grid lg:grid-cols-12'>
-      <div className='relative z-10 lg:col-span-4 lg:col-start-1 lg:row-start-1 lg:bg-transparent lg:py-16'>
-       <div
-        className='absolute inset-x-0 h-1/2 bg-gray-50 lg:hidden'
-        aria-hidden='true'
-       />
-       <div className='mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-none lg:p-0'>
-        <div className='aspect-w-10 aspect-h-6 sm:aspect-w-2 sm:aspect-h-1 lg:aspect-w-1'>
-         <img
-          className='rounded-3xl object-cover object-center shadow-2xl'
-          src='https://images.unsplash.com/photo-1507207611509-ec012433ff52?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=934&q=80'
-          alt=''
-         />
-        </div>
-       </div>
-      </div>
-
-      <div className='relative bg-indigo-600 lg:col-span-10 lg:col-start-3 lg:row-start-1 lg:grid lg:grid-cols-10 lg:items-center lg:rounded-3xl'>
-       <div
-        className='absolute inset-0 hidden overflow-hidden rounded-3xl lg:block'
-        aria-hidden='true'
-       >
-        <svg
-         className='absolute bottom-full left-full translate-y-1/3 -translate-x-2/3 transform xl:bottom-auto xl:top-0 xl:translate-y-0'
-         width={404}
-         height={384}
-         fill='none'
-         viewBox='0 0 404 384'
-         aria-hidden='true'
-        >
-         <defs>
-          <pattern
-           id='64e643ad-2176-4f86-b3d7-f2c5da3b6a6d'
-           x={0}
-           y={0}
-           width={20}
-           height={20}
-           patternUnits='userSpaceOnUse'
-          >
-           <rect
-            x={0}
-            y={0}
-            width={4}
-            height={4}
-            className='text-indigo-500'
-            fill='currentColor'
-           />
-          </pattern>
-         </defs>
-         <rect
-          width={404}
-          height={384}
-          fill='url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)'
-         />
-        </svg>
-        <svg
-         className='absolute top-full -translate-y-1/3 -translate-x-1/3 transform xl:-translate-y-1/2'
-         width={404}
-         height={384}
-         fill='none'
-         viewBox='0 0 404 384'
-         aria-hidden='true'
-        >
-         <defs>
-          <pattern
-           id='64e643ad-2176-4f86-b3d7-f2c5da3b6a6d'
-           x={0}
-           y={0}
-           width={20}
-           height={20}
-           patternUnits='userSpaceOnUse'
-          >
-           <rect
-            x={0}
-            y={0}
-            width={4}
-            height={4}
-            className='text-indigo-500'
-            fill='currentColor'
-           />
-          </pattern>
-         </defs>
-         <rect
-          width={404}
-          height={384}
-          fill='url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)'
-         />
-        </svg>
-       </div>
-       <div className='relative mx-auto max-w-md space-y-6 py-12 px-4 sm:max-w-3xl sm:py-16 sm:px-6 lg:col-span-6 lg:col-start-4 lg:max-w-none lg:p-0'>
-        <h2
-         className='text-3xl font-bold tracking-tight text-white'
-         id='join-heading'
-        >
-         Join our team
-        </h2>
-        <p className='text-lg text-white'>
-         Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id
-         malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames.
-        </p>
-        <a
-         className='block w-full rounded-md border border-transparent bg-white py-3 px-5 text-center text-base font-medium text-indigo-700 shadow-md hover:bg-gray-50 sm:inline-block sm:w-auto'
-         href='#'
-        >
-         Explore open positions
-        </a>
-       </div>
-      </div>
-     </div>
-    </div>
-   </div>
+   
    <SearchBar />
    {searchTerm.length > 0 && (
     <p className='my-4 text-base text-textColor'>
@@ -233,7 +119,38 @@ const Home = () => {
    <div className='w-full h-auto flex items-center justify-evenly gap-4 flex-wrap p-4'>
     <HomeSongContainer musics={filteredSongs ? filteredSongs : allSongs} />
    </div>
-   
+   <div className="relative bg-gray-800">
+      <div className="h-56 bg-indigo-600 sm:h-72 md:absolute md:left-0 md:h-full md:w-1/2">
+        <img
+          className="h-full w-full object-cover"
+          src="https://images.unsplash.com/photo-1525130413817-d45c1d127c42?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=60&blend=6366F1&sat=-100&blend-mode=multiply"
+          alt=""
+        />
+      </div>
+      <div className="relative mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
+        <div className="md:ml-auto md:w-1/2 md:pl-10">
+          <h2 className="text-lg font-semibold text-gray-300">Award winning support</h2>
+          <p className="mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">We’re here to help</p>
+          <p className="mt-3 text-lg text-gray-300">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a
+            scelerisque amet ullamcorper eu enim et fermentum, augue. Aliquet amet volutpat quisque ut interdum
+            tincidunt duis.
+          </p>
+          <div className="mt-8">
+            <div className="inline-flex rounded-md shadow">
+              <a
+                href="#"
+                className="inline-flex items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-gray-900 hover:bg-gray-50"
+              >
+                Visit the help center
+                <ArrowTopRightOnSquareIcon className="-mr-1 ml-3 h-5 w-5 text-gray-400" aria-hidden="true" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer />
   </div>
  );
 };
