688d9a0a45cb16066f2a0bd226c9a1f7559be304
Alejandro Gaerste <alejandrogaerste411@gmail.com>
2022-10-29 20:20:15 +0200

    Fix: style search bar

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -12,13 +12,13 @@ const SearchBar = () => {
 		});
 	};
 	return (
-		<div className='w-full my-4 h-16 bg-card flex items-center justify-center'>
-			<div className='w-full gap-4 p-4 md:w-2/3 bg-primary shadow-xl mt-12 rounded-md flex items-center'>
-				<IoSearch className='text-2xl text-textColor' />
+		<div className='w-full my-4 h-16 bg-layout flex items-center justify-center'>
+			<div className='w-full gap-4 p-4 md:w-2/3 bg-primaryHeader shadow-xl mt-12 rounded-md flex items-center'>
+				<IoSearch className='text-2xl text-white' />
 				<input
 					type='text'
 					value={searchTerm}
-					className='w-full h-full bg-transparent text-lg text-textColor  border-none outline-none '
+					className='w-full h-full bg-transparent text-lg text-white  border-none outline-none '
 					placeholder='Search here ....'
 					onChange={(e) => setSearchTerm(e.target.value)}
 				/>
