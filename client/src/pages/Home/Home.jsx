9bd91c0364db762c3089cfbc23ae9485b8a54602
Marcel Barreiro <marcelbarreiro@gmail.com>
2022-10-30 08:42:51 +0100

    Aplle

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -106,6 +106,13 @@ const Home = () => {
         <div className="relative px-4 py-16 mx-auto overflow-hidden sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="flex flex-col items-center justify-between xl:flex-row">
             <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
+			<div class="flex items-center  -mx-1">
+                        <p class="mx-1 text-sm text-gray-600 dark:text-white">Coming Soon on</p>
+
+                        <svg class="w-6 h-6 mx-1 fill-gray-600 dark:fill-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                            <path d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z" />
+                        </svg>
+                    </div>
               <h2 className=" max-w-lg mb-6 font-sans text-[3.5em] font-bold tracking-tight text-white sm:text-[3.5em] sm:leading-none">
 			  The matching APP<br className="hidden md:block" />
 			  for music lover
@@ -116,6 +123,7 @@ const Home = () => {
               <button class="px-6 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
 			  Find a match now!
 			</button>
+			
             </div>
             <div className="w-full max-w-xl xl:px-8 xl:w-5/12">
               <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
