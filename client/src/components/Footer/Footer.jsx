1284b7a40c90775f28317dc7f4bf200db62e8507
Adrià Vallès Baradad <100840351+adriavallesbaradad@users.noreply.github.com>
2022-10-31 19:47:01 +0100

    trending

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -193,7 +193,7 @@ const Footer = () => {
 								<div className='mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0'>
 									<button
 										type='submit'
-										className='flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-500 py-2 px-4 text-base font-medium text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800'
+										className='flex w-full items-center justify-center rounded-md border border-transparent bg-primaryOrange py-2 px-4 text-base font-medium text-white hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800'
 									>
 										Subscribe
 									</button>
