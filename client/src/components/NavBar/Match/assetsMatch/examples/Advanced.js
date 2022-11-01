77fc108c15c8350e387c3610061367114aeafacd
Marcel Barreiro <marcelbarreiro@gmail.com>
2022-11-01 22:16:47 +0100

    match

--- client/src/components/NavBar/Match/assetsMatch/examples/Advanced.js
+++ client/src/components/NavBar/Match/assetsMatch/examples/Advanced.js
@@ -4,19 +4,19 @@ import TinderCard from "react-tinder-card";
 const db = [
 	{
 		name: "Martha Salazar | 29 years | 70% Match",
-		url: "https://images.pexels.com/photos/1375849/pexels-photo-1375849.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		url: "https://images.pexels.com/photos/247206/pexels-photo-247206.jpeg?auto=compress&cs=tinysrgb&w=800",
 	},
 	{
 		name: "Sofia De la Torre | 29 years | 72% Match",
-		url: "https://images.pexels.com/photos/1375849/pexels-photo-1375849.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		url: "https://images.pexels.com/photos/2061346/pexels-photo-2061346.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
 	},
 	{
 		name: "Natalia Duarte | 32 years | 75% Match",
-		url: "https://images.pexels.com/photos/1375849/pexels-photo-1375849.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		url: "https://images.pexels.com/photos/1152994/pexels-photo-1152994.jpeg?auto=compress&cs=tinysrgb&w=800",
 	},
 	{
 		name: "Daniella Martin | 25 years | 82% Match",
-		url: "https://images.pexels.com/photos/1375849/pexels-photo-1375849.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+		url: "https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg?auto=compress&cs=tinysrgb&w=800",
 	},
 	{
 		name: "Samantha Carvalho | 29 years | 86% Match",
