b4c9fb0c1c9a685aa551ef489bb3c637b34350a8
Marcel Barreiro <marcelbarreiro@gmail.com>
2022-11-01 15:07:49 +0100

    logos

--- client/src/components/NavBar/AboutUs/AboutUs.jsx
+++ client/src/components/NavBar/AboutUs/AboutUs.jsx
@@ -193,7 +193,7 @@ function AboutUs() {
 						<div className='col-span-1 flex justify-center bg-gray-50 py-8 px-8'>
 							<img
 								className='max-h-12'
-								src='https://tailwindui.com/img/logos/transistor-logo-gray-400.svg'
+								src='https://wordpress-797010-2726389.cloudwaysapps.com/wp-content/uploads/2022/11/React-logo-gray-400.png'
 								alt='Workcation'
 							/>
 						</div>
