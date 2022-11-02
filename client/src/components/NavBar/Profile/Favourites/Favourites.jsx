f39752838154fd10f3e32c0c12220fa6399abd08
Marcel Barreiro <marcelbarreiro@gmail.com>
2022-11-02 05:21:04 +0100

    Coming Soon

--- client/src/components/NavBar/Profile/Favourites/Favourites.jsx
+++ client/src/components/NavBar/Profile/Favourites/Favourites.jsx
@@ -104,12 +104,34 @@ function Favourites() {
                 </div>
               </div>
 
-              <a
-                href="#"
-                className="inline-block rounded-md border border-transparent bg-indigo-600 py-3 px-8 text-center font-medium text-white hover:bg-indigo-700"
-              >
-                Shop Collection
-              </a>
+              <div className="mt-8 sm:mx-auto sm:max-w-lg sm:text-center lg:mx-0 lg:text-left">
+                <p className="text-base font-medium text-gray-900">Sign up to get notified when it’s ready.</p>
+                <form action="#" method="POST" className="mt-3 sm:flex">
+                  <label htmlFor="email" className="sr-only">
+                    Email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="block w-full rounded-md border-gray-300 py-3 text-base placeholder-gray-500 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:flex-1"
+                    placeholder="Enter your email"
+                  />
+                  <button
+                    type="submit"
+                    className="mt-3 w-full rounded-md border border-transparent bg-gray-800 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:inline-flex sm:w-auto sm:flex-shrink-0 sm:items-center"
+                  >
+                    Notify me
+                  </button>
+                </form>
+                <p className="mt-3 text-sm text-gray-500">
+                  We care about the protection of your data. Read our
+                  <a href="#" className="font-medium text-gray-900 underline">
+                    Privacy Policy
+                  </a>
+                  .
+                </p>
+              </div>
             </div>
           </div>
         </div>
