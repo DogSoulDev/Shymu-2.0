3a76e17d5353b2bba8d82da4d1b7a566cd568c97
Marcel Barreiro <marcelbarreiro@gmail.com>
2022-11-02 04:54:03 +0100

    player

--- client/src/components/MusicPlayer/MusicPlayer.jsx
+++ client/src/components/MusicPlayer/MusicPlayer.jsx
@@ -72,7 +72,7 @@ const MusicPlayer = () => {
 	}, [song]);
 
 	return (
-		<div className='w-full full flex items-center gap-3 overflow-hidden'>
+		<div className='w-full full flex items-center gap-3 overflow-hidden bg-[#efefef]'>
 			<div
 				className={`w-full full items-center gap-3 p-4 ${
 					miniPlayer ? "absolute top-40" : "flex relative"
