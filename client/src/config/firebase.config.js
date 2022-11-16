69a233187e377e76b388be024b67db16220b3994
Javier Fernández <dogsouldev@gmail.com>
2022-11-16 13:20:37 +0100

    fix firebase

--- client/src/config/firebase.config.js
+++ client/src/config/firebase.config.js
@@ -4,7 +4,6 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
@@ -13,5 +12,4 @@ const firebaseConfig = {
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 const storage = getStorage(app);
-
 export { app, storage };
